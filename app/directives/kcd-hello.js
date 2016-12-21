744dfcaa533fb908662340dc01e0f503a822b098
Zeeshan Syed <zee@percy3d.com>
2016-12-21 13:28:33 -0500

    Added CSS loaders and production ready setup

--- app/directives/kcd-hello.js
+++ app/directives/kcd-hello.js
@@ -1,5 +1,7 @@
 module.exports = function(ngModule) {
   ngModule.directive('kcdHello', function() {
+    require('./kcd-hello.css');
+
     return {
       restrict: 'E',
       scope: {},
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+var webpack = require('webpack');
+
+var config = {
   context: __dirname + '/app',
   entry: './index.js',
   output: {
@@ -17,7 +19,23 @@ module.exports = {
         test: /\.html$/,
         loader: 'raw', // can also use raw-loader
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css', // this loads multiple loaders (right to left), first css loader then style loader
+        exclude: /node_modules/
+      },
+      {
+        test: /\.styl$/,
+        loader: 'style!css!stylus',
+        exclude: /node_modules/
       }
     ]
   }
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.output.path = __dirname + '/dist';
+}
+
+module.exports = config;
